b32dc8f047389d7a942ad24427d6ca255762deed
Erica Schroeder <schroeder.erica.m@gmail.com>
2026-02-22 14:46:41 -0500

    Plant updates and minor tweaks.

--- src/data/forbs.ts
+++ src/data/forbs.ts
@@ -180,4 +180,22 @@ export const forbs: Plant[] = [{
              heightFt: 1.5,
              widthFt: 1,
         },
+},{
+        id: "aquilegia-canadensis",
+        scientificName: "Aquilegia canadensis",
+        commonName: "Wild Columbine",
+        plantType: "forb",
+        sun: ["full", "part", "shade"],
+        soilMoisture: ["med", "dry"],
+        heightFt: {min: 1, max: 3},
+        widthFt: {min: 1, max: 1},
+        flowerColor: ["red"],
+        traits: {
+             keystone: true,
+        },
+        illustration: {
+             svg: "aquilegia-canadensis.svg",
+             heightFt: 2,
+             widthFt: 1,
+        },
 }];
